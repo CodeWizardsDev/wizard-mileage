b4058f929af38e80e97f55069e293130398e2470
Code Wizards <94300419+CodeWizardsDev@users.noreply.github.com>
2025-06-13 12:50:57 +0330

    Update 1.2.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ games { 'gta5' }
 author 'The_Hs5'
 
 description 'Display vehicle mileage, Service vehicle'
-version '1.2.0'
+version '1.2.1'
 ui_page 'html/index.html'
 
 dependency {'oxmysql', 'ox_lib'}
